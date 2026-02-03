639dea4d2b706c849be5316a3679d2cdc14d473c
Akash Tirkey <akash.tirkeywork@gmail.com>
2026-02-04 00:23:29 +0530

    fixed the necessary fixes

--- src/Country.jsx
+++ src/Country.jsx
@@ -4,6 +4,8 @@ import Styles from './Styles/Country.module.css'
 
 export default function Country() {
     const [data, setData] = useState([]);
+    const[loading, setLoading]=useState(true);
+    const[error,setError] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -13,17 +15,24 @@ export default function Country() {
                 setData(response.data);
             } catch (error) {
                 console.log("Couldn't fetch the data", error)
+                setError(true);
+            } finally{
+                setLoading(false);
             }
         }
         fetchData();
     }, []) //this api will be mounted at the start of the application
+
+
+    if(loading) return <p>Loading....</p>
+    if(error) return <p>Something went wrong</p>;
 
     return (
         <>
             <div className={Styles.grid}>
                 {data.map((country) => (
                     <div className={Styles.gridItem} key={country.name}>
-                        <img src={country.flag} alt={country.name} />
+                        <img src={country.flag} alt={`Flag of ${country.name}`} />
                         <p>{country.name}</p>
 
                     </div>
