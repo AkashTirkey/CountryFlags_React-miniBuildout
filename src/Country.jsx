ebf93fd61cfb761e8e89b515af6f1d27e204227c
Akash Tirkey <akash.tirkeywork@gmail.com>
2026-02-04 00:29:14 +0530

    final needed fix

--- src/Country.jsx
+++ src/Country.jsx
@@ -1,43 +1,39 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios'
-import Styles from './Styles/Country.module.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Styles from "./Styles/Country.module.css";
 
 export default function Country() {
-    const [data, setData] = useState([]);
-    const[loading, setLoading]=useState(true);
-    const[error,setError] = useState(false);
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get("https://xcountries-backend.labs.crio.do/all");
-                console.log(response.data)
-                setData(response.data);
-            } catch (error) {
-                console.log("Couldn't fetch the data", error)
-                setError(true);
-            } finally{
-                setLoading(false);
-            }
-        }
-        fetchData();
-    }, []) //this api will be mounted at the start of the application
+  useEffect(() => {
+    axios
+      .get("https://xcountries-backend.labs.crio.do/all")
+      .then((response) => {
+        setCountries(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error); // 🚨 REQUIRED by Cypress
+        setLoading(false);
+      });
+  }, []);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-    if(loading) return <p>Loading....</p>
-    if(error) return <p>Something went wrong</p>;
-
-    return (
-        <>
-            <div className={Styles.grid}>
-                {data.map((country) => (
-                    <div className={Styles.gridItem} key={country.name}>
-                        <img src={country.flag} alt={`Flag of ${country.name}`} />
-                        <p>{country.name}</p>
-
-                    </div>
-                ))}
-            </div>
-        </>
-    )
+  return (
+    <div className={Styles.grid}>
+      {countries.map((country) => (
+        <div className={Styles.gridItem} key={country.name}>
+          <img
+            src={country.flag}
+            alt={`Flag of ${country.name}`} 
+          />
+          <p>{country.name}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
